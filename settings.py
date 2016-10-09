f5d79053577a0e155e803ff5d13ab5c9226eb4e1
Sergey Smirnov <smirnoffs@gmail.com>
2016-10-09 19:51:03 +0200

    fail fast on settings if any necessary value is missing

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 import os
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.environ['SECRET_KEY']
 LANG = os.getenv('LANG', 'en')
-PUSHOVER_TOKEN = os.getenv('PUSHOVER_TOKEN')
-PUSHOVER_KEY = os.getenv('PUSHOVER_KEY')
+PUSHOVER_TOKEN = os.environ['PUSHOVER_TOKEN']
+PUSHOVER_KEY = os.environ['PUSHOVER_KEY']

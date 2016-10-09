0c287ab737e14c741c292474722c2455a9bf7f24
Sergey Smirnov <smirnoffs@gmail.com>
2016-10-09 20:01:39 +0200

    replace environment variable LANG that is used by the system to LANGUAGE

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 import os
 
 SECRET_KEY = os.environ['SECRET_KEY']
-LANG = os.getenv('LANG', 'en')
+LANG = os.getenv('LANGUAGE', 'en')
 PUSHOVER_TOKEN = os.environ['PUSHOVER_TOKEN']
 PUSHOVER_KEY = os.environ['PUSHOVER_KEY']
